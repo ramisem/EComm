3c87c96f19eff4920764e93a0699be224cb5cab3
ramise_mondal <rajaramise@gmail.com>
2024-01-23 00:48:57 +0530

    Modified

--- iotlimsintegrator/iotlimsintegrator/urls.py
+++ iotlimsintegrator/iotlimsintegrator/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import redirect
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('integratorconfig/', admin.site.urls),
+    path('', lambda request: redirect('integratorconfig/')),
 ]
