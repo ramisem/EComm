ec618d135c123649e1238479b8dc1228e5a3e388
ramise_mondal <rajaramise@gmail.com>
2024-02-24 02:44:50 +0530

    Modified

--- iotlimsintegrator/audit/admin.py
+++ iotlimsintegrator/audit/admin.py
@@ -1,9 +1,15 @@
 from auditlog.admin import LogEntryAdmin
+from auditlog.filters import ResourceTypeFilter
 from auditlog.models import LogEntry
 from django.contrib import admin
 
+from audit.forms import AuditSearchForm
+
 
 class CustomLogEntryAdmin(LogEntryAdmin):
+    search_fields = []
+    list_filter = ['object_id', ResourceTypeFilter, 'action']
+    change_form_template = 'admin/audit/audit_change_form.html'
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
         extra_context = extra_context or {}
@@ -15,7 +21,14 @@ class CustomLogEntryAdmin(LogEntryAdmin):
         extra_context['original'] = False
         return super().change_view(request, object_id, form_url, extra_context=extra_context)
 
-    change_form_template = 'admin/audit/audit_change_form.html'
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+    def get_search_fields(self, request):
+        return super().get_search_fields(request)
 
 
 admin.site.unregister(LogEntry)
