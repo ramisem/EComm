be5bceddb6fd458971c3235338b689ec8783fb50
ramise_mondal <rajaramise@gmail.com>
2024-06-15 17:11:29 +0530

    Modified

--- iotlimsintegrator_epam/scripts/create_services.sh
+++ iotlimsintegrator_epam/scripts/create_services.sh
@@ -2,8 +2,6 @@
 
 # Prompt for user input
 # shellcheck disable=SC2162
-read -p "Enter the username to be added to the www-data group: " USERNAME
-# shellcheck disable=SC2162
 read -p "Enter the project directory (e.g., /home/ubuntu/blogprojectdrf): " PROJECT_DIR
 # shellcheck disable=SC2162
 read -p "Enter the Gunicorn socket file path (e.g., /run/gunicorn.sock): " GUNICORN_SOCK
@@ -33,7 +31,7 @@ Requires=gunicorn.socket
 After=network.target
 
 [Service]
-User=$USERNAME
+User=$USER
 Group=www-data
 WorkingDirectory=$PROJECT_DIR
 ExecStart=$PROJECT_DIR/env/bin/gunicorn \\
@@ -59,7 +57,7 @@ setup_nginx() {
     echo "All files in /etc/nginx/sites-enabled/ have been removed."
 
     # Create the Nginx configuration file for the blog
-    sudo tee /etc/nginx/sites-available/blog <<EOL
+    sudo tee /etc/nginx/sites-available/nexus_fusion <<EOL
 server {
     listen 80 default_server;
     server_name _;
@@ -75,30 +73,47 @@ server {
 EOL
 
     # Create a symbolic link to enable the blog site
-    sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
+    sudo ln -s /etc/nginx/sites-available/nexus_fusion /etc/nginx/sites-enabled/
     echo "Nginx site configuration for the blog has been created and enabled."
 
     # Add the user to the www-data group
-    sudo gpasswd -a www-data $USERNAME
-    echo "User $USERNAME has been added to the www-data group."
+    sudo gpasswd -a www-data $USER
+    echo "User $USER has been added to the www-data group."
   else
     echo "Operation cancelled."
     exit 1
   fi
 }
 
+execute_python_services(){
+  application_name="nexus_fusion"
+  application_folder_name="iotlimsintegrator"
+  application_path="/home/$USER/${application_name}"
+  #Project migration and static files handling
+  source ${application_path}/${application_folder_name}/env/bin/activate
+  python ${application_path}/${application_folder_name}/manage.py makemigrations
+  python ${application_path}/${application_folder_name}/manage.py migrate
+  python ${application_path}/${application_folder_name}/manage.py collectstatic
+  deactivate
+}
+
 # Function to restart services
 restart_services() {
   sudo systemctl start gunicorn.socket
   sudo systemctl enable gunicorn.socket
+  sudo systemctl restart redis-server
+  sudo systemctl enable redis-server
+  sudo systemctl status redis-server
   sudo systemctl restart nginx
   sudo service gunicorn restart
   sudo service nginx restart
   echo "Nginx and Gunicorn have been restarted."
 }
 
+
 # Run all setup functions
 setup_gunicorn_socket
 setup_gunicorn_service
 setup_nginx
+execute_python_services
 restart_services
---
+++ iotlimsintegrator_epam/scripts/setup_env.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Prompt for user input
+read -p "Enter the Python version to install (e.g., 3.8): " PYTHON_VERSION
+
+application_name="nexus_fusion"
+application_folder_name="iotlimsintegrator"
+application_path="/home/$USER/${application_name}"
+
+# Add the deadsnakes PPA for Python
+sudo add-apt-repository ppa:deadsnakes/ppa
+
+# Install Python, its development files, virtual environment package, and nginx
+sudo apt install -y python${PYTHON_VERSION} python${PYTHON_VERSION}-dev python${PYTHON_VERSION}-venv nginx
+
+# Download get-pip.py script
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+
+# Install pip for the specified Python version
+sudo python${PYTHON_VERSION} get-pip.py
+
+# Install virtualenv using pip
+python${PYTHON_VERSION} -m pip install virtualenv
+
+if [ ! -d "${application_path}/${application_folder_name}/" ]; then
+  # Create the directory
+  mkdir -p "${application_path}/${application_folder_name}/"
+fi
+
+sudo apt install net-tools
+
+unzip ../iotlimsintegrator.zip -d ${application_path}/${application_folder_name}/
+
+# Create a virtual environment in the specified directory
+python${PYTHON_VERSION} -m virtualenv ${application_path}/${application_folder_name}/env
+
+sudo apt-get install redis-server -y
+
+# Activate the virtual environment
+source ${application_path}/${application_folder_name}/env/bin/activate
+
+pip install -r ${application_path}/${application_folder_name}/requirements.txt
+pip install django gunicorn
+
+
+#Install PostgreSQL
+sudo sh -c 'echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y install postgresql-15
+sudo service postgresql status
